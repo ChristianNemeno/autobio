83b7ba6f9e3e73dc664d7e5ccbf73ff08076e23d
christian <christiannemeno80@gmail.com>
2025-11-01 17:52:58 +0800

    figured how to style using markdown

--- preface.py
+++ preface.py
@@ -1,3 +1,18 @@
 import streamlit as st
 
 st.title("_Hate_ is a place where a man who can't stand :red[sadness] goes")
+
+st.markdown("""
+    <style>
+        div[data-testid="stImageContainer"] img {
+            border-radius: 50% !important;
+            object-fit: cover !important;
+            width: 200px !important;
+            height: 200px !important;
+        }
+    </style>
+""", unsafe_allow_html=True)
+
+col1, col2, col3 = st.columns([1, 2, 1])
+with col2:
+    st.image("resource/profile.jpg")
