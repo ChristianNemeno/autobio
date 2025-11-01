8569fed77f4c2bcb7bbda57e59709ade3f689a2d
christian <christiannemeno80@gmail.com>
2025-11-01 17:56:52 +0800

    adjustment in columns , works like flex but pretty manual

--- preface.py
+++ preface.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.title("_Hate_ is a place where a man who can't stand :red[sadness] goes")
-
+st.title("_Welcome_ to my :red[domain]")
+st.divider()
 st.markdown("""
     <style>
         div[data-testid="stImageContainer"] img {
@@ -13,6 +13,6 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-col1, col2, col3 = st.columns([1, 2, 1])
+col1, col2, col3 = st.columns([2, 1, 2])
 with col2:
     st.image("resource/profile.jpg")
